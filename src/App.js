d8d2ff70de1948e3cb5e46e1175f9ab09df3003a
Kiara <carbonok@uninorte.edu.co>
2021-11-27 17:56:35 -0500

    Instalada plantilla bootstrap

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+          Tanqueo de Gasolina
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
