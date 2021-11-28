580fdc67107e96db44bdbe122116275b1da40557
Kiara <carbonok@uninorte.edu.co>
2021-11-27 21:01:11 -0500

    "leves ajustes al css"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Navbar} from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          Tanqueo de Gasolina
-        </h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <h1>
+            Tanqueo de Gasolina
+          </h1>
+        </header>
+      </div>
+      
+    </>
   );
 }
 
